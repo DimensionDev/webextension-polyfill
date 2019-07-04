e10fd9ef6f89277ea321fcc4481947fa827d2480
Jack Works <zjwpeter@gmail.com>
2019-07-04 15:32:45 +0800

    Fix CI, add missing file

--- src/codegen.js
+++ src/codegen.js
@@ -1,15 +1,29 @@
 const fs = require('fs')
-const codegen = JSON.parse(
+const files = new Set()
+
+for (const each of JSON.parse(
     fs
         .readFileSync('./extension/precache-manifest.js', 'utf-8')
         .replace(/(.+)concat\(/, '')
         .replace(/\);$/, ''),
-)
-    .map(x => x.url)
+).map(x => x.url.replace(/^\//, ''))) {
+    files.add(each)
+}
+
+const manifest = require('./extension/manifest.json')
+for (const each of manifest.content_scripts) {
+    for (const file of each.js) files.add(file)
+}
+
+for (const each of manifest.background.scripts) {
+    files.add(each)
+}
+
+const codegen = Array.from(files)
     .map((url, index) => {
         return `// @ts-ignore
-import $${index} from './extension${url}'
-resources['${url.replace(/^\//, '')}'] = $${index}`
+import $${index} from './extension/${url}'
+resources['${url}'] = $${index}`
     })
     .join('\n')
 
