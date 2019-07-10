4426a2e58d607a44e97db48580df6be10035e97c
Jack Works <zjwpeter@gmail.com>
2019-07-10 14:56:47 +0800

    fix(fetch): fix fetch will send uncloneable object

--- src/shims/fetch.ts
+++ src/shims/fetch.ts
@@ -4,8 +4,8 @@ import { decodeStringOrBlob } from '../utils/StringOrBlob'
 export function createFetch(extensionID: string): typeof fetch {
     return new Proxy(fetch, {
         async apply(target, thisArg, [requestInfo, requestInit]: Parameters<typeof fetch>) {
-            const request = new Request(requestInfo, requestInit)
-            const result = await Host.fetch(extensionID, request)
+            const { body, method, url } = new Request(requestInfo, requestInit)
+            const result = await Host.fetch(extensionID, { method, url })
             const data = await decodeStringOrBlob(result.data)
             if (data === null) throw new Error('')
             const returnValue = new Response(data, result)
