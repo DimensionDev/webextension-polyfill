47460207f8c356efb8076da41eaf92e62c9d4a23
Jack Works <jackworks@protonmail.com>
2020-06-08 14:37:41 +0800

    chore: move original code to eariler position for debug

--- src/static.ts
+++ src/static.ts
@@ -1,82 +1,37 @@
 function static_eval_generated() {
-    const { get, set } = Reflect
-    const currentExtensionGlobal = Symbol.for('GLOBAL_SCOPE')
-    const global = get(globalThis, currentExtensionGlobal)
-    const proxy = new Proxy(
-        { __proto__: null },
-        {
-            get(shadow, prop) {
-                if (typeof prop === 'symbol') {
-                    return undefined
-                }
-                return get(global, prop)
-            },
-            set: (shadow, prop, value) => set(global, prop, value),
-            has: () => true,
-            getPrototypeOf: () => null,
-        },
-    )
     // @ts-ignore
     const value = (function () {
         if (arguments[0]) {
             return function () {
-                // 'use strict'
-                const {
-                    browser,
-                    Infinity,
-                    NaN,
-                    undefined,
-                    isFinite,
-                    isNaN,
-                    parseFloat,
-                    parseInt,
-                    decodeURI,
-                    decodeURIComponent,
-                    encodeURI,
-                    encodeURIComponent,
-                    Array,
-                    ArrayBuffer,
-                    Boolean,
-                    DataView,
-                    EvalError,
-                    Float32Array,
-                    Float64Array,
-                    Int8Array,
-                    Int16Array,
-                    Int32Array,
-                    Map,
-                    Number,
-                    Object,
-                    RangeError,
-                    ReferenceError,
-                    Set,
-                    String,
-                    Symbol,
-                    SyntaxError,
-                    TypeError,
-                    Uint8Array,
-                    Uint8ClampedArray,
-                    Uint16Array,
-                    Uint32Array,
-                    URIError,
-                    WeakMap,
-                    WeakSet,
-                    JSON,
-                    Math,
-                    Reflect,
-                    escape,
-                    unescape,
-                    top,
-                    window,
-                } = globalThis
-                // @ts-ignore
+                // prettier-ignore
+                const { browser, Infinity, NaN, undefined, isFinite, isNaN, parseFloat, parseInt, decodeURI, decodeURIComponent, encodeURI, encodeURIComponent, Array, ArrayBuffer, Boolean, DataView, EvalError, Float32Array, Float64Array, Int8Array, Int16Array, Int32Array, Map, Number, Object, RangeError, ReferenceError, Set, String, Symbol, SyntaxError, TypeError, Uint8Array, Uint8ClampedArray, Uint16Array, Uint32Array, URIError,WeakMap, WeakSet, JSON, Math, Reflect, escape, unescape, top, window, self } = globalThis
                 throw ''
             }.call(undefined)
         }
         // @ts-ignore
-    })(proxy)
+    })(getProxy())
+    function getProxy() {
+        const { get, set } = Reflect
+        const currentExtensionGlobal = Symbol.for('GLOBAL_SCOPE')
+        const global = get(globalThis, currentExtensionGlobal)
+        return new Proxy(
+            { __proto__: null },
+            {
+                get(shadow, prop) {
+                    if (typeof prop === 'symbol') {
+                        return undefined
+                    }
+                    return get(global, prop)
+                },
+                set: (shadow, prop, value) => set(global, prop, value),
+                has: () => true,
+                getPrototypeOf: () => null,
+            },
+        )
+    }
     // Async eval completion
-    get(globalThis, Symbol.for('CALLBACK_HERE'))?.(value)
+    // @ts-ignore
+    globalThis[Symbol.for('CALLBACK_HERE')]?.(value)
     // sync eval completion
     return value
 }
